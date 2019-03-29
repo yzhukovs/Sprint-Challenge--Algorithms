0efcea5c27edac2beb8b711f5249fedb92e2788b
yzhukovs <yzhukovs@bumnetworks.com>
2019-03-29 13:07:40 -0400

    finished MVP

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,22 +96,25 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        self.set_light_on()
-            pass
-        while sel    
-       
-                    
-                    pass
-
-                elif self.compare_item() > 0:
-                    
+        self.set_light_on()	       
+        while self.light_is_on():
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() == 1:
                     self.swap_item()
                     self.move_left()
                     self.swap_item()
                     self.move_right()
                     self.set_light_on()
-                else:    
-                    pass
+                elif self.compare_item() == -1 or self.compare_item() == 0:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+            if self.light_is_on():
+                while self.can_move_left():
+                    self.move_left()
 
 
 if __name__ == "__main__":
